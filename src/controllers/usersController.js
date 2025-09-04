1fbf88b490e8cf340b5ebc8669a47aae0aa84d61
Olaiwonismail <olaiwonismail@gmail.com>
2025-09-04 15:37:03 +0100

    added project structure

---
+++ src/controllers/usersController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
